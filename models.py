2e2e5325eb8164bb9dae4855548a44cfd901a02f
Mathieu Wang <mathieu.sy.wang@gmail.com>
2014-12-01 17:23:18 -0500

    Fix typo in UserAccount comment

--- models.py
+++ models.py
@@ -7,8 +7,7 @@ class UserAccount:
     # @param password Required. The password of the user
     # @param maxLevelReached Default is 1. Used to initialize the user account with a specific unlocked level
     # @param cumulativeScore Default is 0. Used to initialize the user account with a specific cumulative score
-    # @param numGamesPlated Default is 0. Used to initialize the user account with a specific number of games played
-    # @param numGamesPlated Default is 0. Used to initialize the user account with a specific number of games played
+    # @param numGamesPlayed Default is 0. Used to initialize the user account with a specific number of games played
     def __init__(self, username, realname, password, maxLevelReached=1,
             cumulativeScore=0, numGamesPlayed=0,):
         self.username = username
