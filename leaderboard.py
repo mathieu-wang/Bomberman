92ea2628dbd4e87ded259cd2775eb4c8cd82879b
gabriel3g <gibeaultgirardg@outlook.com>
2014-12-01 19:48:53 -0500

    finished documentation for leaderboard

--- leaderboard.py
+++ leaderboard.py
@@ -2,24 +2,35 @@ from PyQt4 import QtCore, QtGui
 
 from database import Database
 
+##This class is a widget that displays the leaderBoard. It includes the following buttons
+#and fields that the user can interact with:\n
+#table: is a field that contains the top 10 players sorted by cumulative scores.
+#backButton: emit backSignal when clicked.
 class Leaderboard(QtGui.QWidget):
 
+    ##Signal which will be used to go back to the previous menu based on an int.
+    #@Param int is set as 'previousMenu'
     backSignal = QtCore.pyqtSignal(int)
 
     def __init__(self, parent, previousMenu):
         super(Leaderboard, self).__init__(parent)
+        ##int used to keep track of the previous menu.
         self.previousMenu = previousMenu
         self.initUI()
+        ##instance of the database.
         self.db = Database()
         self.fillData()
 
+    ##this method initiaze the GUI of the leaderboard.
     def initUI(self):
         vbox = QtGui.QVBoxLayout()
         self.setLayout(vbox)
 
+        ##the number of rows is set to 10.
         self.rowCount = 10
+        ##the number of columns is set to 5
         self.colCount = 5
-
+        ##Is a table of 5 columns (colCount) and 10 rows (rowCount) which contains the top 10 scores.
         self.table = QtGui.QTableWidget()
         self.table.setRowCount(self.rowCount)
         self.table.setColumnCount(self.colCount)
@@ -35,6 +46,8 @@ class Leaderboard(QtGui.QWidget):
         vbox.addWidget(self.table)
         vbox.addWidget(backButton)
 
+    ##this methods fills the data in the table.
+    #users: contains top 10 players sorted by cumulative scores.
     def fillData(self):
         users = self.db.getTopTenUsers()
 
@@ -59,6 +72,6 @@ class Leaderboard(QtGui.QWidget):
             self.table.setItem(i, 3, numGamesItem)
             self.table.setItem(i, 4, cumScoreItem)
             i += 1
-
+    ##emit backSignal when called.
     def back(self):
         self.backSignal.emit(self.previousMenu)
